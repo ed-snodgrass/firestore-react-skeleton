6e37427a27f01c3d32cdae40e5443860aa116278
Ed Snodgrass <Ed.Snodgrass@proxibid.com>
2018-11-13 08:33:13 -0600

    Initial commit

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+const config = {
+  apiKey: '<api-key>',
+  authDomain: '<project-id>.firebaseapp.com',
+  databaseURL: 'https://<project-id>.firebaseio.com',
+  projectId: '<project-id>',
+  storageBucket: '<project-id>.appspot.com',
+  messagingSenderId: '<messagingSenderId>',
+};
+
+export default config;
